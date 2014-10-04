df4ed4e815610fa95ba35d7e5423dcb0ebd27d44
hamidazimy <hamid.azimy@gmail.com>
2014-10-04 12:24:53 +0330

    Refining index.html
    -= portfolio
    += background

--- js/contact-me.js
+++ js/contact-me.js
@@ -1,64 +1,9 @@
-// function send_mail() {
-
-// 	var url = 'https://mandrillapp.com/api/1.0/messages/send.json';
-
-// 	var xhr = new XMLHttpRequest();
-// 	if ("withCredentials" in xhr) {
-// 		// XHR for Chrome/Firefox/Opera/Safari.
-// 		xhr.open(method, url, true);
-// 	} else if (typeof XDomainRequest != "undefined") {
-// 		// XDomainRequest for IE.
-// 		xhr = new XDomainRequest();
-// 		xhr.open(method, url);
-// 	} else {
-// 		// CORS not supported.
-// 		xhr = null;
-// 	}
-
-// 	if (!xhr) {
-// 		console.log('CORS is not supported.');
-// 		return;
-// 	}
-
-// 	// Response handlers.
-
-// 	xhr.onload = function() {
-// 		var text = xhr.responseText;
-// 		console.log('Response from CORS request to ' + url + ': ' + text);
-// 	};
-
-// 	xhr.onerror = function() {
-// 		console.log('Woops, there was an error making the request.');
-// 	};
-
-// 	data = {
-// 		'key'		: '5wl3MmeEtKB4rE9QY6uhdQ',
-// 		'message'	: {
-// 			'from_email'	: 'hamid.azimy@ut.ac.ir',
-// 			'to'			: [
-// 				{
-// 					'email'	: 'hamid.azimy+github.io@gmail.com',
-// 					'name'	: 'Hamid Azimy',
-// 					'type'	: 'to'
-// 				}
-// 			],
-// 			'autotext'	: 'true',
-// 			'subject'	: 'YOUR SUBJECT HERE!',
-// 			'html'		: 'YOUR EMAIL CONTENT HERE! <br /> YOU CAN USE HTML!'
-// 		}
-// 	}
-
-// 	xhr.send(JSON.stringify(data));
-
-// }
-
 $(document).ready(function() {
 
 	$('#send_mail').click(function() {
 
-		// send_mail();
-
 		$('#send_mail').prop('disabled', true);
+		
 		$.ajax({
 			type	: "POST",
 			url		: 'https://mandrillapp.com/api/1.0/messages/send.json',
@@ -82,9 +27,17 @@ $(document).ready(function() {
 
 		}).done(function(response) {
 			console.log(response); // if you're into that sorta thing
+			$('body').append(' \
+				<div id="success_modal" style="display: none;">\
+					Thank you!<br>Your message has been sent.\
+				</div>');
 			$('#success_modal').modal();
 		}).fail(function(response) {
 			console.log(response);
+			$('body').append(' \
+				<div id="failure_modal" style="display: none;">\
+					Oops! There was a problem sending your message.\
+				</div>');
 			$('#failure_modal').modal();
 		}).always(function() {
 			$('#send_mail').prop('disabled', false);
