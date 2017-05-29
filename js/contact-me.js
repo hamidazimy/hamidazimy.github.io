4a76edb8554dd5b03aba520f0a36eff8aee61f66
Hamid Azimy <hamid.azimy@gmail.com>
2017-05-29 11:21:29 -0300

    Fix "contact me" form.

--- js/contact-me.js
+++ js/contact-me.js
@@ -3,28 +3,11 @@ $(document).ready(function() {
 	$('#send_mail').click(function() {
 
 		$('#send_mail').prop('disabled', true);
-		
-		$.ajax({
-			type	: "POST",
-			url		: 'https://mandrillapp.com/api/1.0/messages/send.json',
-			data	: {
-				'key'		: '5wl3MmeEtKB4rE9QY6uhdQ',
-				'message'	: {
-					'from_email'	: $('#email').val(),
-					'from_name'		: $('#name' ).val(),
-					'to'			: [
-						{
-							'email'	: 'hamid.azimy+github.io@gmail.com',
-							'name'	: 'Hamid Azimy',
-							'type'	: 'to'
-						}
-					],
-					'autotext'	: 'true',
-					'subject'	: 'Mail from "' + $('#name' ).val() + '" via github page.' ,
-					'html'		: $('#message').val()	
-				}
-			}
 
+		$.post('https://postmail.invotes.com/send', {
+			"access_token": "ea8yqywkunupp4tdb4foqv2p",
+			"subject": 'Mail from "' + $('#name' ).val() + '" via github page.',
+			"text": 'From: "' + $('#name' ).val() + '" <' + $('#email').val() + '>\n\n\n' + $('#message').val()
 		}).done(function(response) {
 			console.log(response); // if you're into that sorta thing
 			$('body').append(' \
